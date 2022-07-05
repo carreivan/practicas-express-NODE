ab19449e825fbf7ba3ae486c62963e0a7e343c3b
Ivan Carreño <carreaguileraivan@gmail.com>
2022-07-04 22:41:41 -0300

    Trabajando con SEQUELIZE AULA 06 TOTI

--- index.js
+++ index.js
@@ -1,50 +1,32 @@
 const express = require('express')
 const app = express()
+const { Sequelize, DataTypes } = require('sequelize')
+const TaskModel = require('./models/task')
+
+const sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: 'my-database.db'
+})
+
+const tasks = TaskModel(sequelize, DataTypes)
 
 app.set('view engine', 'ejs')
 
-app.get('/fotos', (req, res) => {
-    res.render('fotos', { nome: req.query.nome })
+app.get('/tarefas', async (req, res) => {
+   // const allTasks = await tasks.findAll()
+
+   const allTasks = await sequelize.query('SELECT * FROM Tasks')
+
+    res.json({ allTasks })
 })
 
-app.post('/', (req, res) => {
-    res.send('Respondiendo requisicao POST!')
-})
+app.get('/tarefa/:id', async (req, res) => {
+    const taskId = req.params.id 
+    const task = await tasks.findByPk(taskId)
 
-app.post('/cachorros', (req, res) => {
-    res.send('Adotando un novo cachorro')
+    res.render('tarefa', { id: task.id, name: task.name })
 })
 
 app.listen(8080, () => {
     console.log('Iniciando o servidor express')
 })
-// const server = http.createServer((req, res) => {
-//   if (req.method === 'GET') {
-//     if(req.url ==='/fotos'){ 
-//         res.writeHead(200)
-//         res.end('<html><body><h1>Cheguei na rota de fotos!</h1></body></html>')
-//     }  else if(req.url ==='/cachorros'){ 
-//         res.writeHead(200)
-//         res.end('<html><body><h1>Cheguei na rota dos cachorros!</h1></body></html>')
-//     } else {
-//         res.writeHead(404)
-//         res.end('<html><body><h1>Nao encontrei uma rota!</h1></body></html>')
-
-//     }
-    
-//   } else if (req.method === 'POST') {
-//     res.writeHead(200)
-//     res.end('Respondendo requisicao POST!')
-//   } else if (req.method === 'PUT') {
-//     res.writeHead(200)
-//     res.end('Respondendo requisicao PUT!')
-//   } else if (req.method === 'DELETE') {
-//     res.writeHead(200)
-//     res.end('Respondendo requisicao DELETE!')
-//   } else {
-//     res.writeHead(400)
-//     res.end(`Nao sei tratar esse metodo HTTP: ${req.method}`)
-//   }
-// })
-
- // server.listen(8080)
